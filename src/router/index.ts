253e80545d1eb3db4311bd2a827ee7c8ada1c0b8
GW00265321 <wanliang123>
2025-04-27 15:57:35 +0800

    Switch router to hash history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 // 引入Layout组件
 import Layout from '@/layout/index.vue'
 
@@ -70,7 +70,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
